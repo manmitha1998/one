5f20bbbf4b785312de2e15a608ba04154dacd523
manmitha1998 <34571488+manmitha1998@users.noreply.github.com>
2026-01-28 11:04:13 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins + Maven!");
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class App extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/plain");
+        response.getWriter().println("Hello from Jenkins + Maven WAR!");
     }
 }
